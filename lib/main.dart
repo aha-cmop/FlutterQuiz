7f9487831b2c3e4bf79795cc909d4b36ffcaaf5a
Igor Anokhin <igor.anohin@emlid.com>
2019-09-10 01:06:36 +0300

    add mero

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 
 import "login_page.dart";
 import "event.dart";
+import "home.dart";
 
 void main() {
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
@@ -15,6 +21,30 @@ void main() {
 final FirebaseAuth auth = FirebaseAuth.instance;
 
 class MyApp extends StatelessWidget {
+  Future<String> get _localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    print(directory.path);
+    return directory.path;
+  }
+
+  Future<File> get _localFile async {
+    final path = await _localPath;
+    return File('$path/data.txt');
+  }
+
+  Future<String> readContent() async {
+    try {
+      final file = await _localFile;
+      // Read the file
+      String contents = await file.readAsString();
+      // Returning the contents of the file
+      return contents;
+    } catch (e) {
+      // If encountering an error, return
+      return 'Error!';
+    }
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -31,13 +61,27 @@ class MyApp extends StatelessWidget {
         stream: auth.onAuthStateChanged,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Event();
+            return FutureBuilder(
+              future: readContent(),
+              builder: (BuildContext context, AsyncSnapshot textSnap) {
+                if (textSnap.connectionState == ConnectionState.waiting) {
+                  return Container();
+                } else {
+                  print("TEXTSSSSPNASFASD");
+                  print(textSnap.data);
+                  var content = textSnap.data;
+                  if (content == 'Error!' || content == null)
+                    return Home();
+                  return Event();
+                }
+              }
+            );
           }
           return LoginRegister();
         },
       ),
       routes: <String, WidgetBuilder>{
-        '/home': (BuildContext context) => new Event(),
+        '/home': (BuildContext context) => new Home(),
         '/event': (BuildContext context) => new Event(),
         '/login': (BuildContext context) => new LoginRegister()
       },
