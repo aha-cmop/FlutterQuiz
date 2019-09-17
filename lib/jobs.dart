f7ef1041803f3fa92d3494af171cf8098dd33fc6
Igor Anokhin <igor.anohin@emlid.com>
2019-09-18 00:58:58 +0300

    jobs: add additional info

--- lib/jobs.dart
+++ lib/jobs.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:flutter_html/flutter_html.dart';
 
 class Jobs extends StatefulWidget {
   @override
@@ -9,6 +10,8 @@ class Jobs extends StatefulWidget {
 
 
 class _JobsState extends State<Jobs> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  PersistentBottomSheetController _sheetController;
 
   final url="http://opendata.trudvsem.ru/api/v1/vacancies?offset=1&limit=100";
   TextEditingController editingController = TextEditingController();
@@ -35,11 +38,96 @@ class _JobsState extends State<Jobs> {
     return json.decode(response.body);
   }
 
+  void _jobInfoSheet(vac) {
+    _sheetController = _scaffoldKey.currentState
+        .showBottomSheet<void>((BuildContext context) {
+      return DecoratedBox(
+        decoration: BoxDecoration(color: Theme.of(context).accentColor),
+        child: ClipRRect(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(40.0),
+                topRight: Radius.circular(40.0)),
+            child: Container(
+              child: ListView(
+                children: <Widget>[
+                  Container(
+                    child: Stack(
+                      children: <Widget>[
+                        Positioned(
+                          left: 10,
+                          top: 10,
+                          child: IconButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            icon: Icon(
+                              Icons.close,
+                              size: 30.0,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    height: 50,
+                    width: 50,
+                  ),
+                  SingleChildScrollView(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 6.0),
+                            child: Text(
+                              vac["job-name"],
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontFamily: 'Spectral',
+                                color: Colors.black,
+                                fontSize: 20.0,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 15.0),
+                          Text(
+                            vac["salary"],
+                            style: TextStyle(
+                                fontSize: 17.0,
+                                fontStyle: FontStyle.italic,
+                                color: Colors.pink,
+                                fontFamily: 'Montserrat'),
+                          ),
+                          Container(
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                            padding: EdgeInsets.all(8.0),
+                            child: Html(
+                              data: vac["duty"],
+                            ),
+                          ),
+                        ],
+                      )
+                  ),
+                ],
+              ),
+              height: MediaQuery.of(context).size.height / 1.1,
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+            ),
+          ),
+      );
+    });
+  }
+
+
   Widget _buildJobCard(jsonValue) {
     final vac = jsonValue["vacancy"];
     print("VALUE");
     print(jsonValue.toString());
-    return Card(
+    return GestureDetector(
+      onTap: () {
+        _jobInfoSheet(vac);
+      },
+      child: Card(
         margin: EdgeInsets.all(8.0),
         child: Container(
           margin: const EdgeInsets.only(left: 10.0, top: 5.0, bottom: 5.0),
@@ -57,6 +145,7 @@ class _JobsState extends State<Jobs> {
             trailing: Icon(Icons.info_outline),
           ),
         )
+      ),
     );
   }
 
@@ -65,6 +154,7 @@ class _JobsState extends State<Jobs> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      key: _scaffoldKey,
       appBar: AppBar(
         actions: <Widget>[
           IconButton(
