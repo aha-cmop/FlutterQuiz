eba7aed1ec31898c67d801ba0da5b304bf06ef47
Igor Anokhin <igor.anohin@emlid.com>
2019-09-21 16:25:00 +0300

    myapplication: add notifications

--- myapplication/lib/main.dart
+++ myapplication/lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 
 import "event.dart";
 import 'home.dart';
@@ -19,6 +21,7 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  FirebaseMessaging firebaseMessaging = FirebaseMessaging();
 
   Future<SharedPreferences> _sprefs = SharedPreferences.getInstance();
   bool picked;
@@ -35,6 +38,37 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     getData();
+    firebaseMessaging.configure(
+        onLaunch: (Map<String, dynamic> msg) {
+          print("onLaunch called");
+        },
+        onResume: (Map<String, dynamic> msg) {
+          print("onResume called");
+        },
+        onMessage: (Map<String, dynamic> msg) {
+          print("onMessage called");
+        }
+    );
+    firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(
+            sound: true,
+            alert: true,
+            badge: true
+        )
+    );
+    firebaseMessaging.onIosSettingsRegistered.listen((IosNotificationSettings settings){
+      print('IOS setting registred');
+    });
+    firebaseMessaging.getToken().then((token){
+      update(token);
+    });
+  }
+
+  update(String token) {
+    print("TOKEN: $token");
+    setState(() {
+
+    });
   }
 
   // This widget is the root of your application.
